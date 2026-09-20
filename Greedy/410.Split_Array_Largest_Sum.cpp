02a1439610128c1e3aa30da6d1523ba9afce5f5a
Mayank Jeet <mayankjeet7656@gmail.com>
2026-09-21 00:18:08 +0530

    Add LeetCode 410 solution

---
+++ Greedy/410.Split_Array_Largest_Sum.cpp
@@ -0,0 +1,34 @@
+/*
+ * LeetCode: 410 - Split Array Largest Sum
+ * Link: https://leetcode.com/problems/split-array-largest-sum/
+ * Difficulty: Hard
+ * Time: O(nlog(max-min)) where n is the number of elements in the array and max and min are the maximum and minimum elements in the array
+ * Space: O(1)
+ */
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool isPossible(vector<int>& nums,int maxSum, int k){
+        int n=nums.size(),count=0,sum=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]+sum>maxSum){
+                sum=0;
+                count++;
+            }
+            if(count>=k) return false;
+            sum+=nums[i];
+        }
+        return true;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int low=*max_element(nums.begin(),nums.end());
+        int high=accumulate(nums.begin(),nums.end(),0);
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(isPossible(nums,mid,k)) high=mid-1;
+            else low=mid+1;
+        }
+        return low;
+    }
+};
